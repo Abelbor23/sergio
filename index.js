50ecbeecc54cb8b04ab6bdaabac1785b34e8eab4
Skwrr <64465478+Skwrr@users.noreply.github.com>
2024-09-23 16:53:44 +0200

    comments

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-do {} while (typeof document.getElementById == null)
+do {} while (typeof document.getElementById == null);
 
+// First modification of element with id "clock"
+document.getElementById("clock").innerHTML = new Date().toUTCString();
+
+// Loop every 1 second updating the text of "#clock"
 setInterval(() => {
     document.getElementById("clock").innerHTML = new Date().toUTCString();
 }, 1000)
